009c24338067acfc24c6706130788aa068dba607
zamaslie <54606830+zamaslie@users.noreply.github.com>
2019-10-29 19:19:11 -0400

    Add files via upload

---
+++ max_min_diff/max_min_diff/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace max_min_diff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int the_array = (15, 22, 84, 14, 88, 23);
+
+            int the_highest_value = Math.Max(the_array);
+
+            int the_smallest_number = Math.Min(the_array);
+
+            int the_difference = Math.Abs(the_highest_value - the_smallest_number);
+
+            Console.WriteLine(the_highest_value);
+            Console.WriteLine(the_smallest_number);
+            Console.WriteLine(the_difference);
+        }
+    }
+}
---
+++ max_min_diff/max_min_diff/list.cs
@@ -0,0 +1,6 @@
+﻿namespace max_min_diff
+{
+    internal class list
+    {
+    }
+}
